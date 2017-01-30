6a1af42a99d3bc05b57b2263fcc7f0e26c6b775c
gerardo <gerardo.delarosma@udlap.mx>
2017-01-30 00:13:08 -0600

    Fixing MQ

--- assets/js/admin/LoginController.js
+++ assets/js/admin/LoginController.js
@@ -1,23 +1,18 @@
-angular.module('AteneaApp', []).controller('LoginController', ['$scope', '$http', function($scope, $http) {
-  $scope.user = {};
+angular.module('AteneaApp', []).controller('LoginController', [
+  '$scope',
+  '$http',
+  function($scope, $http) {
+    $scope.user = {};
 
-  $scope.loginEmployee = function(form) {
-    if (form) {
-      console.log($scope.user);
-      $http({
-          method: "PUT",
-          url: '/api/v0/login',
-          data: $scope.user
-        })
-        .then(function successCallback(response) {
+    $scope.loginEmployee = function(form) {
+      if (form) {
+        $http({method: "PUT", url: '/api/v0/login', data: $scope.user}).then(function successCallback(response) {
           var data = response.data;
-          console.log(data);
           if (data.status == 200) {
             window.location.href = "/dashboard";
           }
-        }, function errorCallback(response) {
-          console.log(response);
-        });
+        }, function errorCallback(response) {});
+      }
     }
   }
-}]);
+]);
