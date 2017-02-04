638a36273a922508af73a0be3f8838a50f7b8e1d
GerardoDRM <gerardo.delarosama@udlap.com.mx>
2017-02-04 14:57:13 -0600

    db

--- config/connections.js
+++ config/connections.js
@@ -45,7 +45,7 @@ module.exports.connections = {
     host: 'localhost',
     port: 3306,
     user: 'root', //optional
-    password: 'root', //optional
+    password: 'Judith_2810', //optional
     database: 'stocktakingAtenea' //optional
   },
 
